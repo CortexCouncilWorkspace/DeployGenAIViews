510fed387865adeaa256e1cbc73818186404abe3
bruno-sa02 <83713100+bruno-sa02@users.noreply.github.com>
2024-08-26 10:38:52 -0300

    deploy vf

--- DeployViews.py
+++ DeployViews.py
@@ -24,45 +24,37 @@ class DeployViews:
 
         :param folder_path: Path to the folder containing the SQL scripts.
         """
-        # Inicializar o cliente BigQuery
+        # Start cliente BigQuery
         credentials = service_account.Credentials.from_service_account_file('account/gcp_service_account.json')
         bigquery_client = bigquery.Client(credentials=credentials, project=credentials.project_id)
+        
+        sql_files = sorted([f for f in os.listdir(folder_path) if f.endswith(".sql")])
+        
+        for filename in sql_files:
+            file_path = os.path.join(folder_path, filename)
+            
+            try:
+                with open(file_path, 'r') as file:
+                    sql_script = file.read()
+                    
+                sql_script = sql_script.replace("{{ project_id_tgt }}", self.project_id_tgt)
+                sql_script = sql_script.replace("{{ project_id_src }}", self.project_id_src)                
+                sql_script = sql_script.replace("{{ dataset_reporting_tgt }}", self.dataset_reporting_tgt)
+                sql_script = sql_script.replace("{{ dataset_cdc_processed }}", self.dataset_cdc_processed)
+                sql_script = sql_script.replace("{{ k9_datasets_processing }}", self.k9_datasets_processing)
+                sql_script = sql_script.replace("{{ k9_datasets_reporting }}", self.k9_datasets_reporting)
 
-        sql_files = [f for f in os.listdir(folder_path) if f.endswith(".sql")]
-        sql_files.sort()  # Ordenar os arquivos em ordem alfabética
+                query_job = bigquery_client.query(sql_script)
                 
-        # Iterar sobre os arquivos .sql na pasta
-        for filename in os.listdir(folder_path):
-            if filename.endswith(".sql"):
-                file_path = os.path.join(folder_path, filename)
-                
-                try:
-                    # Ler o conteúdo do arquivo SQL
-                    with open(file_path, 'r') as file:
-                        sql_script = file.read()
-                        
-                    # Substituir os placeholders pelas variáveis reais
-                    sql_script = sql_script.replace("{{ project_id_tgt }}", self.project_id_tgt)
-                    sql_script = sql_script.replace("{{ project_id_src }}", self.project_id_src)                
-                    sql_script = sql_script.replace("{{ dataset_reporting_tgt }}", self.dataset_reporting_tgt)
-                    sql_script = sql_script.replace("{{ dataset_cdc_processed }}", self.dataset_cdc_processed)
-                    sql_script = sql_script.replace("{{ k9_datasets_processing }}", self.k9_datasets_processing)
-                    sql_script = sql_script.replace("{{ k9_datasets_reporting }}", self.k9_datasets_reporting)
+                query_job.result()
 
-                    # Executar o script SQL no BigQuery
-                    query_job = bigquery_client.query(sql_script)
-
-                    # Esperar até a execução ser concluída
-                    query_job.result()
-
-                    print(f"Executed script {filename} successfully.")
-                except FileNotFoundError:
-                    print(f"File {filename} not found.")
-                except Exception as e:
-                    print(f"{filename} - An error occurred: {e}")
+                print(f"Executed script {filename} successfully.")
+            except FileNotFoundError:
+                print(f"File {filename} not found.")
+            except Exception as e:
+                print(f"{filename} - An error occurred: {e}")
+                raise e
                 
-                
-# Instantiate the class
+
 cls = DeployViews()  
 cls.execute_sql_scripts('scripts')
-
